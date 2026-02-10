abe2f9ebf463064f273a665f6dcbe7e28cbaa1f1
ouadal <ouadalissifou7@gmail.com>
2026-02-10 17:21:03 +0000

    Ajout du widget Chatwoot pour le chat en direct

--- app/layout.tsx
+++ app/layout.tsx
@@ -40,6 +40,25 @@ export default function RootLayout({
             `,
           }}
         />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(d,t) {
+                var BASE_URL="https://chatwoot.kya-energy-group.com";
+                var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
+                g.src=BASE_URL+"/packs/js/sdk.js";
+                g.async = true;
+                s.parentNode.insertBefore(g,s);
+                g.onload=function(){
+                  window.chatwootSDK.run({
+                    websiteToken: 'of4MZqErqvWifWMQMi4RKUP2',
+                    baseUrl: BASE_URL
+                  })
+                }
+              })(document,"script");
+            `,
+          }}
+        />
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased bg-white dark:bg-zinc-950 transition-colors`}
